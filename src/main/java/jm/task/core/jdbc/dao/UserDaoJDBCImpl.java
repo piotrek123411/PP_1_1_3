a4b8b1d4f8097688e6a2571bca0cab5f842de61a
piotrek123411 <83031705+piotrek123411@users.noreply.github.com>
2022-11-25 16:01:26 +0300

    a

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -6,6 +6,7 @@ import jm.task.core.jdbc.util.Util;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class UserDaoJDBCImpl implements UserDao {
     private static final Connection conn = Util.getInstance().getConnection();
@@ -17,6 +18,7 @@ public class UserDaoJDBCImpl implements UserDao {
         try (Statement statement = conn.createStatement()) {
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS users " +
                     "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), last_name VARCHAR(255), age INT)");
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -25,6 +27,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public void dropUsersTable() {
         try (Statement statement = conn.createStatement()) {
             statement.executeUpdate("DROP TABLE IF EXISTS users");
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -36,6 +39,7 @@ public class UserDaoJDBCImpl implements UserDao {
             pstm.setString(2, lastName);
             pstm.setByte(3, age);
             pstm.executeUpdate();
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -45,6 +49,7 @@ public class UserDaoJDBCImpl implements UserDao {
         try (PreparedStatement pstm = conn.prepareStatement("DELETE FROM users WHERE id = ?")) {
             pstm.setLong(1, id);
             pstm.executeUpdate();
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -52,26 +57,28 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        Statement statement = conn.createStatement();
-        try (ResultSet resultSet = statement.executeQuery("SELECT * FROM users")){
-            while(resultSet.next()) {
-                User user = new User(resultSet.getString("name"),
-                        resultSet.getString("last_name"), resultSet.getByte("age"));
-                user.setId(resultSet.getLong("id"));
-                users.add(user);
+        try(Statement statement = conn.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM users")){
+                while(resultSet.next()) {
+                    User user = new User(resultSet.getString("name"),
+                            resultSet.getString("last_name"), resultSet.getByte("age"));
+                    user.setId(resultSet.getLong("id"));
+                    users.add(user);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            statement.close();
             conn.close();
-        }
+        } catch (Exception e) {
+                e.printStackTrace();
+            }
         return users;
     }
 
     public void cleanUsersTable() {
         try (Statement statement = conn.createStatement()) {
             statement.executeUpdate("TRUNCATE TABLE users");
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
